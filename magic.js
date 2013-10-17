9c19e94ef91e7fa86c0007d3f8614d487dd70d7b
Joah Gerstenberg <joahd@gbergfamily.com>
2013-10-17 16:34:43 -0500

    Init commit - v1.0.2

---
+++ magic.js
@@ -0,0 +1,7 @@
+var addButtons = function(){
+	//nothing yet....
+}
+
+var script = document.createElement("script");
+script.textContent = "(" + addButtons.toString() + "())";
+document.documentElement.appendChild(script);
